e0aea56983d4fb22e4c9e49e48fbc89f6a16efb7
Brea Zeller <38115861+bzeller7@users.noreply.github.com>
2019-04-29 15:09:20 -0700

    Edited the DateType for DateTime so only the calender can be shown when making a task

--- ToDoApp/Models/ToDo.cs
+++ ToDoApp/Models/ToDo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace ToDoApp
 {
     public class ToDo
     {
+        [Key]
         public int ID { get; set; }
 
         public string Name { get; set; }
@@ -15,6 +17,7 @@ namespace ToDoApp
 
         public string Priority { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime DueDate { get; set; }
     }
 }
