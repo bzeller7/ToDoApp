b11b0ae57681137a433fca61f15c174c97af5123
Brea Zeller <38115861+bzeller7@users.noreply.github.com>
2019-06-05 13:44:19 -0700

    Working on establishing the database for the uncompleted tasks

--- ToDoApp/Controllers/ToDoController.cs
+++ ToDoApp/Controllers/ToDoController.cs
@@ -3,18 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Models;
 
 namespace ToDoApp.Controllers
 {
     public class ToDoController : Controller
     {
+        private readonly ToDoContext context;
+
+        public ToDoController(ToDoContext dbContext)
+        {
+            context = dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<ToDo> toDos = TaskDB.GetToDos(context);
+            return View(toDos);
         }
+
         public IActionResult CreateTask()
         {
             return View();
         }
+        [HttpGet]
+        public IActionResult CreateTask(ToDo task)
+        {
+            if (ModelState.IsValid)
+            {
+                TaskDB.AddTask(task, context);
+
+                ViewData["Message"] = "the task was added!";
+            }
+            return View(task);
+        }
+        //public IActionResult Edit(string title)
+        //{
+        //    //get the product by id
+        //    ToDo task = TaskDB.GetToDos(context, title);
+
+        //    //show it on the web page
+        //    return View(task);
+        //}
     }
 }
---
+++ ToDoApp/Models/TaskDB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class TaskDB
+    {
+        public static ToDo AddTask(ToDo task
+                            , ToDoContext context)
+        {
+            context.ToDos.Add(task);
+            context.SaveChanges();
+
+            return task;
+
+        }
+        internal static List<ToDo>GetToDos(ToDoContext context)
+        {
+            return (from task in context.ToDos
+                    select task).ToList();
+        }
+    }
+}
